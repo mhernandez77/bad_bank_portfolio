5a4f9602b919704c90dca934e984d8759cb0b475
unknown <you@example.com>
2023-02-06 22:39:00 -0600

    state & bootstrap added

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get("/account/all", function (req, res) {
 });
 
 // Listening for the port
-var port = process.env.PORT || 5000;
-app.listen(port, function () {
-    console.log("Server is running on port: " + port);
-});
+
+var port = 3000;
+app.listen(port);
+console.log('Running on port: ' + port);
